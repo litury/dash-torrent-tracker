106523c28652c145b6457b7b33223e3d270d2d65
Litvinov Y. <litury@icloud.com>
2025-12-12 16:20:56 +0500

    feat(deps): add sonner, react-hook-form and clsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'sonner'
 import './main.css'
 
 import { Layout } from './shared/components/Layout'
 import { HomePage, AddTorrentPage } from './config/routes'
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="add" element={<AddTorrentPage />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="add" element={<AddTorrentPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    <Toaster position="bottom-right" richColors />
+  </>
 )
 
 const rootElement = document.getElementById('root')
